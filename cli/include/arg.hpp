a1ff0101086b045f8c0a6ba3f65cc4304e3a95a4
STupidLord <st.business.contact@gmail.com>
2026-08-17 02:19:26 -0400

    Removed some commented code in arg.hpp

--- cli/include/arg.hpp
+++ cli/include/arg.hpp
@@ -73,36 +73,4 @@ command_return parse_args(ini::ini& config,
                           const std::vector<std::string_view>& args);
 int handle_error(const command_return& result,
                  const std::vector<std::string_view>& args);
-
-
-
-
-// enum class argv_error {
-//     ERR_SUCCESS                = 0,
-//     ERR_ARG                    = 1,
-//     ERR_ARG_SIZE               = 2,
-//     ERR_ARG_CONFLICT           = 3,
-//     ERR_ARG_EXPECTED_VALUE     = 4,
-//     ERR_ARG_EXPECTED_VALUE_GOT = 5
-// };
-
-// struct argv_flags {
-//     bool print_version;
-//     bool print_help;
-// };
-
-// struct argv_values {
-//     std::optional<std::filesystem::path> game_directory;
-//     std::optional<std::filesystem::path> user_directory;
-// };
-
-// struct argv_result {
-//     argv_error                      error_code;
-//     std::optional<std::string_view> bad_arg;
-//     std::optional<std::string_view> bad_arg_2;
-//     argv_flags                      flags;
-//     argv_values                     values;
-// };
-
-// argv_result parse_args(const std::vector<std::string_view> args);
 };
